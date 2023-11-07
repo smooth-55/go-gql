6422ce470460b9324d2f2c3a5f6cd9c22bf923aa
Ashish <dhakalashish18@gmail.com>
2023-11-07 21:06:02 +0545

    feat: user query

---
+++ apps/user/queries.go
@@ -0,0 +1,87 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/graphql-go/graphql"
+)
+
+type Query struct {
+	types UserTypes
+}
+
+func NewQuery(
+	types UserTypes,
+
+) Query {
+	return Query{
+		types: types,
+	}
+}
+
+func (q Query) GetAllUsersResolver(p graphql.ResolveParams) (interface{}, error) {
+	users := []User{
+		{
+			Id:       1,
+			Username: "username",
+			FullName: "full name",
+			Phone:    "98xxxxxx",
+		},
+		{
+			Id:       2,
+			Username: "username2",
+			FullName: "full name2",
+			Phone:    "98xxxxxx",
+		},
+	}
+	return users, nil
+}
+
+func (q Query) GetAllUserField() *graphql.Field {
+	return &graphql.Field{
+		Type:    graphql.NewList(q.types.GetAllUserType("GetAllUser")),
+		Args:    nil,
+		Resolve: q.GetAllUsersResolver,
+	}
+
+}
+
+func (q Query) GetOneUserResolver(p graphql.ResolveParams) (interface{}, error) {
+	Id := p.Args["id"].(string)
+
+	users := []User{
+		{
+			Id:       1,
+			Username: "username",
+			FullName: "full name",
+			Phone:    "98xxxxxx",
+		},
+		{
+			Id:       2,
+			Username: "username2",
+			FullName: "full name2",
+			Phone:    "98xxxxxx",
+		},
+	}
+	for _, user := range users {
+		if fmt.Sprintf("%v", user.Id) == Id {
+			return user, nil
+		}
+	}
+	return nil, errors.New("User doesn't exists")
+}
+
+func (q Query) GetOneUserField() *graphql.Field {
+	return &graphql.Field{
+		Type: q.types.GetAllUserType("GetOneUser"),
+		Args: graphql.FieldConfigArgument{
+
+			"id": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+		},
+		Resolve: q.GetOneUserResolver,
+	}
+
+}
