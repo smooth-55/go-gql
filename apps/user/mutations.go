00b1c9deeda1288e1c9d9b93d1507f43b2581a34
Ashish <dhakalashish18@gmail.com>
2023-11-07 21:05:52 +0545

    feat: user mutation

---
+++ apps/user/mutations.go
@@ -0,0 +1,40 @@
+package user
+
+import "github.com/graphql-go/graphql"
+
+type Mutation struct {
+	types UserTypes
+}
+
+func NewMutation(
+	types UserTypes,
+) Mutation {
+	return Mutation{
+		types: types,
+	}
+}
+
+func (m Mutation) CreateUserResolver(p graphql.ResolveParams) (interface{}, error) {
+	inputs := p.Args["input"].(map[string]interface{})
+	user := User{
+		Id:       1,
+		Username: inputs["username"].(string),
+		FullName: inputs["full_name"].(string),
+		Phone:    inputs["phone"].(string),
+	}
+	return user, nil
+}
+
+func (m Mutation) CreateUserField() *graphql.Field {
+	return &graphql.Field{
+		Type:    m.types.UserResponseType(),
+		Resolve: m.CreateUserResolver,
+		Args: graphql.FieldConfigArgument{
+			"input": &graphql.ArgumentConfig{
+				Type:        m.types.CreateUserInputType(),
+				Description: "this is des",
+			},
+		},
+		Description: "This mutation is used to create a new user",
+	}
+}
